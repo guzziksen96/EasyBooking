5189745fa5b83f37da82dff385c97122943fd713
guzziksen96 <dyska96@interia.pl>
2018-05-01 23:21:06 +0200

    changing models, adding Faker.Net and NBuilder libraries and creating seeds, fixing the structure

---
+++ EasyBooking/Models/ViewModels/Schedule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace EasyBooking.Models.ViewModels
+{
+    [Table("schedules")]
+    public class Schedule
+    {
+        [Key]
+        public int Id { get; set; }
+        public int FirstClassRemainingSeats { get; set; }
+        public int EconomyClassRemainingSeats { get; set; }
+        public double FirstClassPrice { get; set; }
+        public double EconomyClassPrice { get; set; }
+        public virtual ICollection<Reservation> Reservations { get; set; }
+
+    }
+}
