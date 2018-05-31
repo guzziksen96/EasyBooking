89a35771c6ca82a2be20e5831517f582171c9af4
guzziksen96 <dyska96@interia.pl>
2018-05-31 17:40:32 +0200

    Adding list of matching flights on new page after clicking Search button

--- EasyBooking/Controllers/HomeController.cs
+++ EasyBooking/Controllers/HomeController.cs
@@ -18,6 +18,32 @@ namespace EasyBooking.Controllers
         {
             return View();
         }
-        
+
+        [HttpPost]
+        public ActionResult SearchResults(string departureCity, string arrivalCity, DateTime fromDate, DateTime returnDate)
+        {
+            ViewBag.Source = departureCity;
+            ViewBag.Dest = arrivalCity;
+
+            if (DateTime.Compare(fromDate, DateTime.Today) > 0)
+            {
+                var flights = db.Flights.Where(f => f.ArrivalCity == arrivalCity && f.DepartureCity == departureCity
+                                                && f.DepartureDate == fromDate && f.ArrivalDate == returnDate).ToList();
+
+                if (flights.Count() == 0)
+                {
+                    ViewBag.ScheduleMessage = "No flights on the entered date";
+
+                }
+                return View(flights);
+            }
+            else
+            {
+                ViewBag.ScheduleMessage = "Cannot book flights for requested date. Choose date after today";
+
+                return View();
+            }
+
+        }
     }
 }
